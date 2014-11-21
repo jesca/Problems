14914540607098a50354bb517f5074045316bfbd
jesca <jesca116@gmail.com>
2014-11-21 15:47:10 -0800

    added car class

---
+++ car.py
@@ -0,0 +1,21 @@
+class Car:
+        def __init__(self, model, color):
+                self.model = model
+                self.distance = 0.0
+                self.color = color
+
+
+        def getDistance():
+                #return the total distance that the car has driven
+
+        def getModel():
+                #returns the car’s model
+
+        def getColor():
+               # return the color of the car.
+
+
+        def drive(distance):
+               # when user ‘drives’, then we want to increase the variable ‘totalDistance’ by the distance driven
+
+                                
