c04e0c401243abf0c1d0b5c818c95d5ab1235421
jesca <jesca116@gmail.com>
2014-11-21 15:48:40 -0800

    added car

--- car.py
+++ car.py
@@ -7,15 +7,16 @@ class Car:
 
         def getDistance():
                 #return the total distance that the car has driven
+                pass
 
         def getModel():
                 #returns the car’s model
+                pass
 
         def getColor():
                # return the color of the car.
-
+               pass
 
         def drive(distance):
                # when user ‘drives’, then we want to increase the variable ‘totalDistance’ by the distance driven
-
-                                
+               pass
